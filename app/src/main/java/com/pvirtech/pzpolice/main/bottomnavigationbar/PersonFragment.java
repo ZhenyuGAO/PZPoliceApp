5777bf672158d9d6b3580ba3a2dcbf83e16946b1
18380163461 <3340418505@qq.com>
2017-02-27 17:39:36 +0800

    a

--- app/src/main/java/com/pvirtech/pzpolice/main/bottomnavigationbar/PersonFragment.java
+++ app/src/main/java/com/pvirtech/pzpolice/main/bottomnavigationbar/PersonFragment.java
@@ -2,24 +2,50 @@ package com.pvirtech.pzpolice.main.bottomnavigationbar;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.pvirtech.pzpolice.R;
 import com.pvirtech.pzpolice.ui.base.BaseFragment;
+import com.pvirtech.pzpolice.ui.holder.FirstTreeItemHolder;
+import com.pvirtech.pzpolice.ui.holder.SecondTreeItemHolder;
+import com.unnamed.b.atv.model.TreeNode;
+import com.unnamed.b.atv.view.AndroidTreeView;
 
 /**
  * 人员薄主页
  */
 public class PersonFragment extends BaseFragment {
     private Context mContext = null;
+    private AndroidTreeView tView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_task, container, false);
+        View view = inflater.inflate(R.layout.fragment_person, container, false);
         mContext = getActivity();
         initView(view);
+
+        ViewGroup containerView = (ViewGroup) view.findViewById(R.id.container);
+        TreeNode root = TreeNode.root();
+        TreeNode socialNetworks = new TreeNode(new FirstTreeItemHolder.IconTreeItem(R.mipmap.person, "Social")).setViewHolder(new FirstTreeItemHolder(getActivity()));
+        TreeNode facebook = new TreeNode(new SecondTreeItemHolder.IconTreeItem(R.mipmap.person, "Social1")).setViewHolder(new SecondTreeItemHolder(getActivity()));
+        TreeNode linkedin = new TreeNode(new SecondTreeItemHolder.IconTreeItem(R.mipmap.person, "Social2")).setViewHolder(new SecondTreeItemHolder(getActivity()));
+        TreeNode google = new TreeNode(new SecondTreeItemHolder.IconTreeItem(R.mipmap.person, "Social3")).setViewHolder(new SecondTreeItemHolder(getActivity()));
+        TreeNode twitter = new TreeNode(new SecondTreeItemHolder.IconTreeItem(R.mipmap.person, "Social4")).setViewHolder(new SecondTreeItemHolder(getActivity()));
+        socialNetworks.addChildren(facebook, google, twitter, linkedin);
+        root.addChildren(socialNetworks);
+        tView = new AndroidTreeView(getActivity(), root);
+        tView.setDefaultAnimation(true);
+        tView.setDefaultContainerStyle(R.style.TreeNodeStyleDivided, true);
+        containerView.addView(tView.getView());
+        if (savedInstanceState != null) {
+            String state = savedInstanceState.getString("tState");
+            if (!TextUtils.isEmpty(state)) {
+                tView.restoreState(state);
+            }
+        }
         return view;
     }
 
@@ -28,4 +54,5 @@ public class PersonFragment extends BaseFragment {
     public void onDestroyView() {
         super.onDestroyView();
     }
+
 }
---
+++ app/src/main/java/com/pvirtech/pzpolice/ui/holder/FirstTreeItemHolder.java
@@ -0,0 +1,55 @@
+package com.pvirtech.pzpolice.ui.holder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.pvirtech.pzpolice.R;
+import com.unnamed.b.atv.model.TreeNode;
+
+
+/**
+ * Created by Bogdan Melnychuk on 2/12/15.
+ */
+public class FirstTreeItemHolder extends TreeNode.BaseNodeViewHolder<FirstTreeItemHolder.IconTreeItem> {
+    private TextView tvTitle;
+    private ImageView iv_Nav;
+
+    public FirstTreeItemHolder(Context context) {
+        super(context);
+    }
+
+    @Override
+    public View createNodeView(final TreeNode node, IconTreeItem value) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(com.pvirtech.pzpolice.R.layout.layout_tree_first, null, false);
+        tvTitle = (TextView) view.findViewById(R.id.tv_title);
+        tvTitle.setText(value.text);
+
+        ImageView iconView = (ImageView) view.findViewById(R.id.iv_icon);
+        iconView.setImageResource(value.icon);
+        iv_Nav = (ImageView) view.findViewById(R.id.iv_nav);
+        if (node.isLeaf()) {
+            iv_Nav.setVisibility(View.INVISIBLE);
+        }
+        return view;
+    }
+
+    @Override
+    public void toggle(boolean active) {
+        System.out.println("aaaaaaaaa");
+        iv_Nav.setImageResource(active ? R.mipmap.down : R.mipmap.right);
+    }
+
+    public static class IconTreeItem {
+        public int icon;
+        public String text;
+
+        public IconTreeItem(int icon, String text) {
+            this.icon = icon;
+            this.text = text;
+        }
+    }
+}
