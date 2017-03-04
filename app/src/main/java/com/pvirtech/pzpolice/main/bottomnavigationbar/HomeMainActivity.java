0156460c011db003c8d6d5c0df2656eb3abbebb2
18380163461 <3340418505@qq.com>
2017-03-04 15:38:10 +0800

    a

--- app/src/main/java/com/pvirtech/pzpolice/main/bottomnavigationbar/HomeMainActivity.java
+++ app/src/main/java/com/pvirtech/pzpolice/main/bottomnavigationbar/HomeMainActivity.java
@@ -5,8 +5,9 @@ import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,9 +17,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.ashokvarma.bottomnavigation.BadgeItem;
-import com.ashokvarma.bottomnavigation.BottomNavigationBar;
-import com.ashokvarma.bottomnavigation.BottomNavigationItem;
 import com.pvirtech.pzpolice.R;
 import com.pvirtech.pzpolice.file.down.DownloadActivity;
 import com.pvirtech.pzpolice.file.down.DownloadService;
@@ -29,19 +27,34 @@ import java.util.ArrayList;
 import butterknife.ButterKnife;
 
 
-public class HomeMainActivity extends AppCompatActivity implements BottomNavigationBar.OnTabSelectedListener, NavigationView.OnNavigationItemSelectedListener {
+public class HomeMainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private ArrayList<Fragment> fragments;
     TaskFragment taskFragment = new TaskFragment();
     WorkFragment workFragment = new WorkFragment();
     PersonFragment personFragment = new PersonFragment();
     ScoreboardFragment scoreboardFragment = new ScoreboardFragment();
-    /**
-     * 设置显示主页
-     */
-    SettingFragment mSettingFragment = new SettingFragment();
-    String[] titles = {"我的任务", "我的考勤","人员簿", "积分榜"};
     Toolbar toolbar;
     private Context mContext = null;
+
+
+    private void onTableSelected(Fragment fragment, String title) {
+        FragmentManager fm = HomeMainActivity.this.getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if (fragment.isAdded()) {
+            ft.replace(R.id.layFrame, fragment);
+        } else {
+            ft.add(R.id.layFrame, fragment);
+        }
+        ft.commitAllowingStateLoss();
+        toolbar.setTitle(title);
+
+       /* private void setDefaultFragment() {
+            FragmentManager fm = HomeMainActivity.this.getFragmentManager();
+            FragmentTransaction transaction = fm.beginTransaction();
+            transaction.replace(R.id.layFrame, taskFragment);
+            transaction.commit();
+        }*/
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,101 +66,52 @@ public class HomeMainActivity extends AppCompatActivity implements BottomNavigat
 
         ButterKnife.bind(this);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle(titles[0]);//设置主标题
+        toolbar.setTitle("我的任务");//设置主标题
         setSupportActionBar(toolbar);
         getSupportActionBar().setHomeButtonEnabled(true); //设置返回键可用
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
+        /**
+         * 左侧抽提
+         */
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.app_name, R.string.app_name);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name, R.string.app_name);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-
-        BottomNavigationBar bottomNavigationBar = (BottomNavigationBar) findViewById(R.id.bottom_navigation_bar);
-        bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
-        bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
-        BadgeItem numberBadgeItem = new BadgeItem()
-                .setBorderWidth(4)
-                .setBackgroundColor(Color.RED)
-                .setText("5")
-                .setHideOnSelect(true);
-        bottomNavigationBar.addItem(new BottomNavigationItem(R.mipmap.task, mContext.getResources().getString(R.string.task)).setActiveColorResource(R.color.button_theme)
-                .setBadgeItem(numberBadgeItem))
-                .addItem(new BottomNavigationItem(R.mipmap.attendance, mContext.getResources().getString(R.string.attendance)).setActiveColorResource(R.color.button_theme))
-                .addItem(new BottomNavigationItem(R.mipmap.mail_list, mContext.getResources().getString(R.string.person)).setActiveColorResource(R.color.button_theme))
-                .addItem(new BottomNavigationItem(R.mipmap.scoreboard, mContext.getResources().getString(R.string.scoreboard)).setActiveColorResource(R.color.button_theme))
-//                .addItem(new BottomNavigationItem(R.mipmap.ic_videogame_asset_white_24dp, "Games").setActiveColorResource(R.color.grey)
-// .setBadgeItem(numberBadgeItem))
-                .setFirstSelectedPosition(0)
-                .initialise();
-
-        fragments = getFragments();
-        setDefaultFragment();
-        bottomNavigationBar.setTabSelectedListener(this);
+        /**
+         * 底部导航
+         */
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.task:
+                        onTableSelected(taskFragment, "我的任务");
+                        return true;
+                    case R.id.attendance:
+                        onTableSelected(workFragment, "我的考勤");
+                        return true;
+                    case R.id.mail_list:
+                        onTableSelected(personFragment, "人员簿");
+                        return true;
+                    case R.id.scoreboard:
+                        onTableSelected(scoreboardFragment, "积分榜");
+                        return true;
+                }
+                return false;
+            }
+        });
+        onTableSelected(taskFragment, "我的任务");
     }
 
     /**
      * 设置默认的
      */
-    private void setDefaultFragment() {
-        FragmentManager fm = HomeMainActivity.this.getFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        transaction.replace(R.id.layFrame, taskFragment);
-        transaction.commit();
-    }
 
 
-    private ArrayList<Fragment> getFragments() {
-        ArrayList<Fragment> fragments = new ArrayList<>();
-        fragments.add(taskFragment);
-        fragments.add(workFragment);
-        fragments.add(personFragment);
-        fragments.add(scoreboardFragment);
-        return fragments;
-    }
-
-    @Override
-    public void onTabSelected(int position) {
-        if (fragments != null) {
-            if (position < fragments.size()) {
-                FragmentManager fm = HomeMainActivity.this.getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                Fragment fragment = fragments.get(position);
-                if (fragment.isAdded()) {
-                    ft.replace(R.id.layFrame, fragment);
-                } else {
-                    ft.add(R.id.layFrame, fragment);
-                }
-                ft.commitAllowingStateLoss();
-                toolbar.setTitle(titles[position]);
-            }
-        }
-
-    }
-
-    @Override
-    public void onTabUnselected(int position) {
-        if (fragments != null) {
-            if (position < fragments.size()) {
-                FragmentManager fm = HomeMainActivity.this.getFragmentManager();
-                FragmentTransaction ft = fm.beginTransaction();
-                Fragment fragment = fragments.get(position);
-                ft.remove(fragment);
-                ft.commitAllowingStateLoss();
-            }
-        }
-    }
-
-    @Override
-    public void onTabReselected(int position) {
-
-    }
 
 
     @Override
@@ -203,7 +167,6 @@ public class HomeMainActivity extends AppCompatActivity implements BottomNavigat
         } else if (id == R.id.nav_send) {
 
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
